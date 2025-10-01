0d108f74944ac36ae371a185a347646caba45e0c
Rajeswariponduru <kmaha39693@gmail.com>
2025-10-01 18:58:18 +0530

    Update dayone.py

--- dayone.py
+++ dayone.py
@@ -5,3 +5,22 @@ for i in range(1,1001):
     print(flower[i-1])
 for i in range(1,1000):
  print(i)
+
+for i in range(1,6):
+    print(i)
+    
+j=1
+while(j<6):
+    print(j)
+    j+=1
+    
+k=1
+while k<51:
+    print(k)
+    k+=2
+    
+n=int(input("Enter a number"))
+if n%2==0:
+    print("It is an even number")
+else:
+    print("it is an odd number")

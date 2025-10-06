29559872d9bb3213848184481ed2110e982f9ce5
Rajeswariponduru <kmaha39693@gmail.com>
2025-10-06 18:16:18 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -1,3 +1,5 @@
+oops concept
+
 class person:
    def __init__(self,name,age):
        self.name = name
@@ -8,3 +10,14 @@ class person:
         
 person= person("Raji",27)
 print(person)
+
+class dog:
+   def __init__(self,name,age):
+       self.name = name
+       self.age = age
+    
+   def __str__(self):
+    return f"{self.name},{self.age} years old "
+        
+dog= dog("chintu",1)
+print(dog)
